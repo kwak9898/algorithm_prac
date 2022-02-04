b3459ba2f67a39a37ed5a0d617d272cf7573a481
kwak9898 <80763040+kwak9898@users.noreply.github.com>
2022-02-04 17:20:24 +0900

    핸드폰 번호 가리기

---
+++ 9_phoneNumber.js
@@ -0,0 +1,16 @@
+function solution(phone_number) {
+    let answer = ''
+
+    for (let i = 0; i < phone_number.length; i++) {
+        if (i < phone_number.length - 4) {
+            answer += '*'
+        } else {
+            answer += phone_number[i]
+        }
+    }
+
+    return answer
+}
+
+console.log(solution("01033334444"))
+console.log(solution("027778888"))
