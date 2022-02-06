fdaaeff2926228ee20ac215ff0cc49ef76514d15
kwak9898 <80763040+kwak9898@users.noreply.github.com>
2022-02-06 19:49:38 +0900

    수박수박수박수박수박수?

---
+++ 19_watermelon.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let answer = ''
+
+    for (let i = 1; i < n + 1; i++) {
+        if (i % 2 === 0) {
+            answer += "박"
+        } else {
+            answer += "수"
+        }
+    }
+    return answer
+}
+
+console.log(solution(3))
+console.log(solution(4))
